1932099a4c43c32561104f15ebdf878c39c75b4d
leeziyou <leeziyou@163.com>
2022-12-15 20:11:24 +0800

    discuss and update for microphone publish

--- src/xf_mic_asr_offline_line/src/limo_commands.cpp
+++ src/xf_mic_asr_offline_line/src/limo_commands.cpp
@@ -22,12 +22,14 @@ int awake_flag = 0;    //唤醒标志位
 int confidence_threshold ;
 int seconds_per_order ;
 int recognize_fail_count = 0;  //被动休眠相关变量
+
+
 /**************************************************************************
-函数功能：唤醒标志sub回调函数
+函数功能：唤醒标志subscribe回调函数
 入口参数：唤醒标志位awake_flag_msg  voice_control.cpp
 返回  值：无
 **************************************************************************/
-void awake_flag_Callback(std_msgs::Int8 msg)
+void awake_flag_callback(std_msgs::Int8 msg)
 {
 	awake_flag = msg.data;
 	printf("awake_flag=%d\n",awake_flag);
@@ -52,7 +54,13 @@ int main(int argc, char *argv[])
 	string str4 = "失败5次";				//语音识别相关字符串
 	string str5 = "失败10次";				//语音识别相关字符串
 
-	ros::init(argc, argv, "call_rec");    //初始化ROS节点
+	// IoT commands words
+	string str_IoT_word_L0 = "关闭左灯";
+	string str_IoT_word_L1 = "打开左灯";
+	string str_IoT_word_R0 = "关闭右灯";
+	string str_IoT_word_R1 = "打开右灯";
+
+	ros::init(argc, argv, "limo_voice_recognize_commands_publish");    //初始化ROS节点
 	ros::NodeHandle nh;    //创建句柄
  
 	/***离线命令词识别服务客户端创建***/
@@ -64,13 +72,16 @@ int main(int argc, char *argv[])
 	//nh.serviceClient<xf_mic_asr_offline_line::Set_Awake_Word_srv>("xf_asr_offline_node/set_awake_word_srv");
 
 	/***唤醒标志位话题订阅者创建***/
-	ros::Subscriber awake_flag_sub = nh.subscribe("awake_flag", 1, awake_flag_Callback);
+	ros::Subscriber awake_flag_sub = nh.subscribe("awake_flag", 1, awake_flag_callback);
 
 	/***唤醒标志位话题发布者创建***/
 	ros::Publisher awake_flag_pub = nh.advertise<std_msgs::Int8>("awake_flag", 1);
 
 	/***离线命令词识别结果话题发布者创建***/
 	ros::Publisher control = nh.advertise<std_msgs::String>("voice_words", 1);
+
+	/***IoT控制命令字符串发布***/
+	ros::Publisher IoT_commands_pub = nh.advertise<std_msgs::String>("IoT_commands", 1)
 
 	ros::Rate loop_rate(10);    //循环频率10Hz
 
@@ -100,47 +111,42 @@ int main(int argc, char *argv[])
 			if(get_offline_recognise_result_client.call(GetOfflineResult_srv))    //请求离线命令词识别服务并返回应答为调用成功
 			{
 				//ROS_INFO("succeed to call service \"get_offline_recognise_result_srv\"!");    //打印识别结果、置信度、识别命令词等信息
+				std::cout << "iFLYTEK offline ASR:" << endl;
 				std::cout << "result: " << GetOfflineResult_srv.response.result << endl;
 				std::cout << "fail reason: " << GetOfflineResult_srv.response.fail_reason << endl;
 				std::cout << "text: " << GetOfflineResult_srv.response.text << endl;
 
-				if(str3 == GetOfflineResult_srv.response.text)    // "小车休眠" -> 主动休眠
-				{ 
-					awake_flag=0;
-					recognize_fail_count = 0;
-					
-				}
-				else if(str1 == GetOfflineResult_srv.response.result)    //清零被动休眠相关变量
-					recognize_fail_count = 0;
-				else if(str2 == GetOfflineResult_srv.response.result)    //记录识别失败次数
+				// voice words compare and publish to later Android
+				std_msgs::String command;
+				if(str_IoT_word_L0 == Get_Offline_Result_srv.response.text)
 				{
-					recognize_fail_count++;
-					//printf("recognize_fail_count=%d\n",recognize_fail_count);
-					if(recognize_fail_count==5)    //连续识别失败5次，用户界面显示提醒信息
-					{
-						std_msgs::String count_msg;
-						count_msg.data = str4;
-						control.publish(count_msg);
-						
-					}
-					else if(recognize_fail_count==10)    //连续识别失败10次，用户界面显示提醒信息
-					{
-						std_msgs::String count_msg;
-						count_msg.data = str5;
-						control.publish(count_msg);
-						
-					}
-					else if(recognize_fail_count >= recognize_fail_count_threshold)    //被动休眠
-					{
-						awake_flag=0;
-						std_msgs::String controloff_msg;
-						controloff_msg.data = str3;
-						control.publish(controloff_msg);
-						recognize_fail_count = 0;
-					
-					}
+					command.data = "L0";
 				}
+
+				else if(str_IoT_word_L1 == Get_Offline_Result_srv.response.text)
+				{
+					command.data = "L1";
+				}
+
+				else if(str_IoT_word_R0 == Get_Offline_Result_srv.response.text)
+				{
+					command.data = "R0";
+				}
+
+				else if(str_IoT_word_R1 == Get_Offline_Result_srv.response.text)
+				{
+					command.data = "R1";
+				}
+
+				else
+				{
+					command.data = "";
+					printf("voice words command not recognized or pre-set.");
+				}
+				IoT_commands_pub.publish(command);
+
 			}
+
 			else        //请求离线命令词识别服务并返回应答为调用失败
 			{
 				ROS_INFO("failed to call service \"get_offline_recognise_result_srv\"!");
