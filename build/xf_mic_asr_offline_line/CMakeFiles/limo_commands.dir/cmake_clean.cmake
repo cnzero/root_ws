f11f93df7d7588fcefbf87669f6cef6447eb0122
liziyou <liziyou>
2023-01-13 17:04:38 +0800

    20230113T1705

---
+++ build/xf_mic_asr_offline_line/CMakeFiles/limo_commands.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/agilex/root_ws/devel/lib/xf_mic_asr_offline_line/limo_commands"
+  "/home/agilex/root_ws/devel/lib/xf_mic_asr_offline_line/limo_commands.pdb"
+  "CMakeFiles/limo_commands.dir/src/limo_commands.cpp.o"
+  "CMakeFiles/limo_commands.dir/src/limo_commands.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/limo_commands.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/xf_mic_asr_offline_line/CMakeFiles/limo_microphone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/agilex/root_ws/devel/lib/xf_mic_asr_offline_line/limo_microphone"
+  "/home/agilex/root_ws/devel/lib/xf_mic_asr_offline_line/limo_microphone.pdb"
+  "CMakeFiles/limo_microphone.dir/src/limo_microphone.cpp.o"
+  "CMakeFiles/limo_microphone.dir/src/limo_microphone.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/limo_microphone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
